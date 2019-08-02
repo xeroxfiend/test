9f2633aa1023a2ee96d158ff8da05b0d6b3be75b
Brian Lowe <mynameisboy@gmail.com>
2019-08-02 16:51:49 -0600

    Learned about loops

--- testfile.js
+++ testfile.js
@@ -1,3 +1,7 @@
+//number adder
+
+console.log("number adder\n")
+
 function addNumbers(a,b) {
     return a+b; 
 }
@@ -5,3 +9,21 @@ function addNumbers(a,b) {
 console.log(addNumbers(1,2));
 console.log(addNumbers(10,20));
 console.log(addNumbers(74,120));
+
+//loops
+
+console.log("\nloops\n")
+
+let myArray = []
+
+for(let b = 0; b < 11; b++) {
+    myArray.push(b)
+}
+
+//myArray.fill(1,0,11)
+
+console.log("Array = ", myArray)
+
+let newArray = myArray.slice(6,11)
+
+console.log(myArray, newArray)
